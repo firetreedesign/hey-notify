48eae5f8fd3f310d7368dba93b4c6d7be7121e09
Daniel Milner <dmilner@gmail.com>
2020-07-22 11:23:48 -0700

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -12,19 +12,19 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-$remove_data = get_option( '_hey_notify_remove_data' );
+$remove_data = \get_option( '_hey_notify_remove_data' );
 
 if ( 'yes' === $remove_data ) {
-	delete_option( '_hey_notify_remove_data' );
-	delete_option( '_hey_notify_default_service' );
+	\delete_option( '_hey_notify_remove_data' );
+	\delete_option( '_hey_notify_default_service' );
 
-	$all_posts = get_posts(
+	$all_posts = \get_posts(
 		array(
 			'post_type'   => 'hey_notify',
 			'numberposts' => -1,
 		)
 	);
 	foreach ( $all_posts as $single_post ) {
-		wp_delete_post( $single_post->ID, true );
+		\wp_delete_post( $single_post->ID, true );
 	}
 }
