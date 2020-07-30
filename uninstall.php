31f6e0d632d12238586456300c74ea427c2f7f74
Daniel Milner <dmilner@gmail.com>
2020-07-30 11:23:47 -0700

    Uninstall should now work with multisite

--- uninstall.php
+++ uninstall.php
@@ -12,12 +12,51 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-$remove_data = \get_option( '_hey_notify_remove_data' );
+if ( \is_multisite() ) {
+	$sites = \get_sites(
+		array(
+			'number' => 99999,
+			'fields' => 'ids',
+		)
+	);
+	if ( $sites ) {
+		foreach ( $sites as $site ) {
+			\switch_to_blog( $site->blog_id );
+			$remove_data = \get_option( '_hey_notify_remove_data' );
+			if ( 'yes' === $remove_data ) {
+				delete_options();
+				delete_posts();
+			}
+			\restore_current_blog();
+		}
+	}
+} else {
+	$remove_data = \get_option( '_hey_notify_remove_data' );
+	if ( 'yes' === $remove_data ) {
+		delete_options();
+		delete_posts();
+	}
+}
 
-if ( 'yes' === $remove_data ) {
+/**
+ * Delete options
+ *
+ * @return void
+ */
+function delete_options() {
 	\delete_option( '_hey_notify_remove_data' );
 	\delete_option( '_hey_notify_default_service' );
+	\delete_option( 'hey_notify_wordpress_version' );
+	\delete_option( 'hey_notify_theme_versions' );
+	\delete_option( 'hey_notify_plugin_versions' );
+}
 
+/**
+ * Delete posts
+ *
+ * @return void
+ */
+function delete_posts() {
 	$all_posts = \get_posts(
 		array(
 			'post_type'   => 'hey_notify',
