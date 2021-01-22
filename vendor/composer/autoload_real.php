c20fde915e1b05982c593ef3e1d19227e6240b7a
Daniel Milner <dmilner@gmail.com>
2021-01-22 12:56:24 -0800

    Updated vendor files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,19 +13,24 @@ class ComposerAutoloaderInitc4d1016cfbfc548fc9f1275a2706c10b
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitc4d1016cfbfc548fc9f1275a2706c10b', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitc4d1016cfbfc548fc9f1275a2706c10b', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitc4d1016cfbfc548fc9f1275a2706c10b::getInitializer($loader));
         } else {
---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.1.5.x-dev',
+    'version' => '1.1.5.9999999-dev',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b57d1880d1b08fe658c81e7c07c0d97c48816e8c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.1.5.x-dev',
+      'version' => '1.1.5.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b57d1880d1b08fe658c81e7c07c0d97c48816e8c',
+    ),
+    'htmlburger/carbon-fields' => 
+    array (
+      'pretty_version' => 'v3.2.1',
+      'version' => '3.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cd772f1c6c002207eed71f98a8c54ad9403d5644',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.5.8',
+      'version' => '3.5.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9d583721a7157ee997f235f327de038e7ea6dac4',
+    ),
+  ),
+);
