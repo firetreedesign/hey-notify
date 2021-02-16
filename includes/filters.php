4889b9d976f368e7d5ae1b1e1282ae4c39b5c7ce
Daniel Milner <dmilner@gmail.com>
2021-02-16 14:36:56 -0800

    Updated section headings in settings

--- includes/filters.php
+++ includes/filters.php
@@ -73,13 +73,7 @@ function event_fields( $fields = array() ) {
  */
 function settings_uninstall_fields( $fields = array() ) {
 	$fields[] = (
-		Field::make( 'html', 'hey_notify_uninstall_heading' )
-			->set_html(
-				sprintf(
-					'<h2>%1s</h2>',
-					__( 'Upon deletion of the plugin, you can optionally remove all custom data, settings, etc.', 'hey-notify' )
-				)
-			)
+		Field::make( 'separator', 'hey_notify_uninstall_separator', __( 'Upon deletion of the plugin, you can optionally remove all custom data, settings, etc.', 'hey-notify' ) )
 	);
 	$fields[] = (
 		Field::make( 'checkbox', 'hey_notify_remove_data', __( 'Remove all data when Hey Notify is deleted.', 'hey-notify' ) )
@@ -95,26 +89,14 @@ function settings_uninstall_fields( $fields = array() ) {
  */
 function settings_general_fields( $fields = array() ) {
 	$fields[] = (
-		Field::make( 'html', 'hey_notify_services_heading' )
-			->set_html(
-				sprintf(
-					'<h2>%1s</h2>',
-					__( 'General settings for Hey Notify', 'hey-notify' )
-				)
-			)
+		Field::make( 'separator', 'hey_notify_general_separator', __( 'General settings', 'hey-notify' ) )
 	);
 	$fields[] = (
 		Field::make( 'radio_image', 'hey_notify_default_service', __( 'Default service:', 'hey-notify' ) )
 			->set_options( get_service_options() )
 	);
 	$fields[] = (
-		Field::make( 'html', 'hey_notify_services_cpt_heading' )
-			->set_html(
-				sprintf(
-					'<h2>%1s</h2>',
-					__( 'Custom Post Type Settings', 'hey-notify' )
-				)
-			)
+		Field::make( 'separator', 'hey_notify_cpt_separator', __( 'Custom Post Type Settings', 'hey-notify' ) )
 	);
 	$fields[] = (
 		Field::make( 'checkbox', 'hey_notify_show_public_cpt', __( 'Only display public Custom Post Types', 'hey-notify' ) )
